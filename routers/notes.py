20837909df7bf1727d890b87d4daa27e0b24a1c6
Tuyishime Johnson <j.tuyishime4@alustudent.com>
2026-04-23 07:14:51 +0200

    add background task

--- routers/notes.py
+++ routers/notes.py
@@ -1,5 +1,7 @@
+from anyio import sleep
 from fastapi import APIRouter
-from fastapi import HTTPException
+from time import sleep
+from fastapi import HTTPException, BackgroundTasks
 from fastapi.responses import JSONResponse
 from app.schemas import NoteCreate
 
@@ -7,57 +9,67 @@ from app.schemas import NoteCreate
 router = APIRouter()
 
 notes = []
+
+
+def custom_success_message(message: str):
+    sleep(4)
+    return JSONResponse(content={"message": message})
+
+
 @router.post("/notes", response_model=NoteCreate)
 # get the ids increment by 1 while creating a note
+async def create_note(note: NoteCreate, background_tasks: BackgroundTasks):
 
-
-def create_note(note: NoteCreate):
-    if not notes:
-        note.id = 1
+    background_tasks.add_task(
+        custom_success_message, f"note id:{note.id} created successfully"
+    )
+    if len(notes) == 0:
+        note.id = len(notes) + 1
     else:
-        note.id = len(list(n.id for n in notes)) + 1
+        note.id = notes[-1].id + 1
 
     notes.append(note)
     return note
 
 
 @router.get("/notes")
-def read_notes():
+async def read_notes():
     return notes
 
 
 @router.get("/notes/{id}", response_model=NoteCreate)
-def read_note(note_id: int):
+async def read_note(note_id: int, background_tasks: BackgroundTasks):
     for note in notes:
         if note.id == note_id:
+            background_tasks.add_task(
+                custom_success_message, f"note id:{note_id} retrieved successfully"
+            )
             return note
-        raise HTTPException(
-            status_code=404, detail=f"Note with id:{note_id} is not found"
-        )
-    return None
+    raise HTTPException(status_code=404, detail=f"Note with id:{note_id} is not found")
 
 
 @router.put("/notes/{id}", response_model=NoteCreate)
-def update_note(note_id: int, updated_note: NoteCreate):
+async def update_note(
+    note_id: int, updated_note: NoteCreate, background_tasks: BackgroundTasks
+):
     for note in notes:
-        if note.id != note_id:
-            raise HTTPException(
-                status_code=404, detail=f"Note with id:{note_id} not found"
+        if note.id == note_id:
+            background_tasks.add_task(
+                custom_success_message, f"note id:{note_id} updated successfully"
             )
-        note.id = note_id
-        note.title = updated_note.title
-        note.content = updated_note.content
+            note.title = updated_note.title
+            note.content = updated_note.content
         return note
-    return None
+    raise HTTPException(status_code=404, detail=f"Note with id:{note_id} not found")
 
 
 @router.delete("/notes/{id}", response_model=NoteCreate)
-def delete_note(note_id: int):
+async def delete_note(note_id: int, background_tasks: BackgroundTasks):
     for note in notes:
         if note.id == note_id:
+            background_tasks.add_task(
+                custom_success_message, f"note id:{note_id} deleted successfully"
+            )
             notes.remove(note)
-            return note
-        raise HTTPException(
-            status_code=404, detail=f"Note with id:{note_id}not found"
-        )
-    return None
+        return note
+    raise HTTPException(status_code=404, detail=f"Note with id:{note_id}not found")
